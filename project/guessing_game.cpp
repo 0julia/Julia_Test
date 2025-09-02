1e61495c7f10aa5432557c74d929229cc7b97276
0julia <126218398+0julia@users.noreply.github.com>
2025-09-02 10:52:45 -0700

    adding comments guessing_game.cpp

--- project/guessing_game.cpp
+++ project/guessing_game.cpp
@@ -2,18 +2,28 @@
 #include <cstdlib>
 using namespace std;
 /*
+  Julia Larson
+  9/2/25
   program ranomly chooses a number
   user guesses a number
   program prints out if number is too big/small
   if it is the number, program asks player if they want to play again
+
+  Rules
+  Treat your teachers and classmates with respect
+  Be safe
+  Use your class time well
 */
 
 
 int main()
 {
+  int guesses = 1;
   srand(time(NULL));
   int answer = rand() % 101;
   int guess = 101;
+  
+  //While you haven't guesses correctly...
   while (guess != answer){
     cout<< "what is your guess? ";
     cin>>guess;
@@ -23,8 +33,14 @@ int main()
     if (guess > answer){
       cout << "Too high!" <<endl;
     }
+    guesses++; 
   }
+  
+  //once you guessed the correct number...
   cout << "YAY!!! You gessed correctly!" << endl;
+  cout << "gessed in " << guesses << " gueseses." << endl;
+  
+  // if you want to play again
   char play;
   cout<< "Do you want to play agian?( y or n) ";
   cin>>play;
