5e1a7357e7e9df0835aa452b3f232e6d697084d3
U-BSD\455929 <455929@SHS-9YBF6X3.beaverton.k12.or.us>
2025-09-10 09:36:17 -0700

    Final draft of palindrome

--- project/palindrome.cpp
+++ project/palindrome.cpp
@@ -1,7 +1,15 @@
+/*
+  Julia Larson
+  9/10/25
+  This will read in a series of 19 or less characters
+  it will print "palindrome" if the string is a palindrome and "not palindrome" if it is not
+*/
+
 #include <iostream>
 #include <algorithm> // Required for std::reverse
 #include <vector>
 #include <cstring>
+#include <cctype>
 using namespace std;
 
 int main()
@@ -11,29 +19,31 @@ int main()
   char newstr[len];
   cin.get(str, len);
   int count = 0;
+
+  //vis wil creat a knew streeng wifowt punkuation!!!!!!
   for (int i = 0; i < len; i++){
-    if (str[i] != '.' && str[i] != ',' && str[i] != ';' && str[i] != ':' && str[i] != '\0' && str[i] != '(' && str[i] != ')' && str[i] != '[' && str[i] != ']' && str[i] != '/' && str[i] != ' '){
-      newstr[count] = str[i];
+    if (str[i] != '.' && str[i] != ',' && str[i] != ';' && str[i] != ':' && str[i] != '(' && str[i] != ')' && str[i] != '[' && str[i] != ']' && str[i] != '/' && str[i] != ' '){
+      newstr[count] = tolower(str[i]);
       count++;
     }
   }
+  
   len = strlen(newstr);
-  cout << "Length of string: " << len << endl;
-  char rvsstr[len];
+  char rvsstr[len+1];
   count = 0;
-  for (int i =0; i<=len; i++){
+  
+  //mayk and reavers the streang
+  for (int i =0; i <= len; i++){
     if (newstr[len-i] != '\0'){
       rvsstr[count] = newstr[len-i];
       count++;
     }
   }
+  rvsstr[count] = '\0';
   if (strcmp(newstr, rvsstr) == 0){
     cout << "It is a palindrome!!!" << endl;
   }else{
     cout << "It's not a palindrome!" << endl;
   }
-  cout << "length of rvs string: " << strlen(rvsstr) << endl;
-  cout<< newstr <<endl;
-  cout << "new string: " << rvsstr << endl;
   return 0;
 }
